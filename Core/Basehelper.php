cb222037600018ef9c32f3752d383b8dc61e7cf1
Tinkqa <ganna_de_jonge@hotmail.com>
2016-05-21 16:44:38 +0200

    lala verwijderd
    .

--- Core/Basehelper.php
+++ Core/Basehelper.php
@@ -11,8 +11,7 @@ class BaseHelper{
 		{
 			global $host, $user, $db_password, $database;
 			$cxn = mysqli_connect($host,$user,$db_password,$database) or die ("Query died: connect");
-			return $cxn;
-			//lalala
+			return $cxn;			
 		}
 	}
 
